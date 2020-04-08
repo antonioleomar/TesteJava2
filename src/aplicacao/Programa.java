32959b363d3dab81e312a3307c9f75fc2772571a
Antonio Leomar <antonioleomarsoares@gmail.com>
2020-04-07 21:38:00 -0300

    Bom dia 2

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -5,8 +5,6 @@ public class Programa {
 	public static void main(String[]args) {
 		
 		System.out.println("Bom dia");
-		System.out.println("Bom tarde");
-		System.out.println("Boa noite");
 	}
 
 }
